17f44abd614929574d6e789518948fbaeba9fe8f
tryiou <73646876+tryiou@users.noreply.github.com>
2024-02-11 17:23:27 +0100

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Use the official Python 3.10 image as base
+FROM python:3.10
+
+# Set working directory in the container
+WORKDIR /app
+
+# Copy the Python files and requirements.txt into the container
+COPY cg_proxy_xrs.py .
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose port 8080
+EXPOSE 8080
+
+# Command to run the Python script
+CMD ["python", "cg_proxy_xrs.py"]
+
---
+++ cg_proxy_xrs.py
@@ -0,0 +1,160 @@
+import asyncio
+import time
+import requests
+from aiohttp import web
+import json
+
+# Cache to store the fetched JSON data
+data_cache = {"cg_coins_list": {}, "cg_data": {}}
+
+import requests
+import time
+
+
+async def fetch_and_parse_json(url, max_retries=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for bad status codes
+            data = response.json()
+            return data
+        except Exception as e:
+            print(f"Error fetching data: {e}")
+            retries += 1
+            if retries <= max_retries:
+                print(f"Retrying in {30 * retries} seconds... (Attempt {retries}/{max_retries})")
+                await asyncio.sleep(30 * retries)  # Wait for 20 seconds before retrying
+    print(f"Maximum number of retries ({max_retries}) reached. Unable to fetch data.")
+    return None
+
+
+async def update_coingecko_coins_list():
+    print("Starting update_coingecko_coins_list task...")
+    while True:
+        try:
+            # Fetch data from the CoinGecko API
+            url = "https://api.coingecko.com/api/v3/coins/list"
+            data = await fetch_and_parse_json(url)
+            timestamp = time.time()
+            data_cache["cg_coins_list"] = {"data": data, "timestamp": timestamp}
+            print("cg_coins_list data updated")
+        except Exception as e:
+            print("Error updating cg_coins_list data:", e)
+        await asyncio.sleep(60 * 60)
+
+
+async def update_coingecko_coins_tickers():
+    print("Starting update_coingecko_coins_tickers task...")
+    final_data = {}  # Initialize an empty dictionary to accumulate all response data
+    while True:
+        try:
+            if data_cache and 'cg_coins_list' in data_cache:
+                coin_ids = [entry['id'] for entry in data_cache['cg_coins_list']['data']]
+                chunk_start_index = 0
+                while chunk_start_index < len(coin_ids):
+                    chunk_end_index = chunk_start_index
+                    while chunk_end_index < len(coin_ids):
+                        chunk_ids = coin_ids[chunk_start_index:chunk_end_index + 1]
+                        ids_string = ','.join(chunk_ids)
+                        url_template = "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd&include_24hr_vol=true"
+                        url = url_template.format(ids_string)
+                        if len(url) > 8000:
+                            # If the URL length exceeds the limit, remove the last coin ID and retest
+                            chunk_end_index -= 1
+                            break
+                        else:
+                            chunk_end_index += 1
+                    # Make the API call with the constructed ids_string
+                    chunk_ids = coin_ids[chunk_start_index:chunk_end_index + 1]
+                    ids_string = ','.join(chunk_ids)
+                    url = url_template.format(ids_string)
+                    data = await fetch_and_parse_json(url)
+                    print(chunk_start_index, chunk_end_index, len(chunk_ids), len(coin_ids))
+                    # Update the final_data dictionary with the data from the current API call
+                    timestamp = time.time()
+                    for key in data:
+                        data[key]["timestamp"] = timestamp
+                    print("updated data for :", chunk_ids)
+                    data_cache['cg_data'].update(data)
+                    # Move to the next chunk
+                    chunk_start_index = chunk_end_index + 1
+                    await asyncio.sleep(15)
+                print(data_cache['cg_data'])
+        except Exception as e:
+            print("Error updating cg_coins_tickers data:", e)
+            await asyncio.sleep(15)
+        else:
+            await asyncio.sleep(15)
+
+
+async def cg_coins_list():
+    print("Request received for call_number1")
+    if data_cache and "cg_coins_list" in data_cache:
+        return {"success": True, "reply": data_cache['cg_coins_list']}
+    else:
+        return {"success": False, "reply": "No data to serve"}
+
+
+async def cg_coins_data(coins):
+    print("cg_coins_data", coins)
+    if not isinstance(coins, list) or not all(isinstance(coin, str) for coin in coins):
+        return {"success": False, "reply": "Invalid parameters: coins must be a list of strings"}
+
+    result = {}
+    for coin in coins:
+        coin = coin.lower()
+        if coin in data_cache['cg_data']:
+            result[coin] = data_cache['cg_data'][coin]
+        else:
+            result[coin] = {"code": 404, "error": "requested coin not in cache"}
+    if result:
+        return {"success": True, "reply": result}
+    else:
+        return {"success": False, "reply": "error gathering requested coin(s) data"}
+
+
+async def start_server():
+    print("Starting JSON-RPC server...")
+    app = web.Application()
+    app.router.add_post('/', handle_request)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8080)
+    await site.start()
+
+
+async def handle_request(request):
+    try:
+        request_data = await request.json()
+        method_name = request_data.get('method') if 'method' in request_data else None
+        params = request_data.get('params') if 'params' in request_data else None
+        if method_name:
+            if method_name == 'cg_coins_list':
+                result = await cg_coins_list()
+                return web.json_response(result)
+            elif method_name == 'cg_coins_data':
+                result = await cg_coins_data(params)
+                return web.json_response(result)
+            else:
+                raise ValueError("Method not found")
+        else:
+            raise ValueError("Method not provided")
+    except Exception as e:
+        return web.json_response({"success": False, "reply": str(e)}, status=400)
+
+
+async def main():
+    # Start the update_coingecko functions as a separate tasks
+    asyncio.create_task(update_coingecko_coins_list())
+    asyncio.create_task(update_coingecko_coins_tickers())
+
+    # Start the JSON-RPC server in a dedicated async function
+    asyncio.create_task(start_server())
+
+
+if __name__ == '__main__':
+    print("Starting main function...")
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
